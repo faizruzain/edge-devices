f235e9f8d4a58494b3e5355f28f6083e633a196d
faizruzain <faizruzain@hotmail.com>
2026-02-10 21:14:08 +0700

    added user input

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 import pandas as pd
-import re
+# import sys
+# sys.path.append('../my_modules')
+from my_modules.module_1 import BCS
 
-df = pd.read_csv('csv/vEdge-02-10-2026.csv')
-df = df.get(['Hostname', 'Version', 'Reachability', 'Subject SUDI serial #'])
-print(df)
-print('\n')
-router_LA = []
-dc_drc_LA_pattern = "(^DC|DRC).*(LA|LA\\d+)$"
-
-router_TLK = []
-dc_drc_TLK_pattern = "(^DC|DRC).*(TLK|TLK\\d+)$"
-
-router_PINS = []
-dc_drc_PINS_pattern = "(^DC|DRC).*(PINS|PINS\\d+)$"
-
-version_pattern = "17\\.12.*"
-
-for row in df.itertuples(index=False):
-    hostnames = row[0]
-    version = row[1]
-    status = row[2]
-    if isinstance(hostnames, float) == False and re.match(version_pattern, str(version), flags=0) and status == 'reachable':
-        match = re.match(dc_drc_LA_pattern, hostnames, flags=0)
-        if match:
-            result = {
-                'hostname': row[0],
-                'version': row[1],
-                'SN': row[3]
-            }
-            router_LA.append(result)
-            # print(hostnames)
-
-print(f'Total Router LA: {len(router_LA)}')
-router_LA_df = pd.DataFrame(data=router_LA)
-print(router_LA_df)
-router_LA_df.to_csv(path_or_buf='output/3.csv',index=False, mode='x')
+def main():
+    df = pd.read_csv('input/vEdge-02-10-2026.csv')
+    print('Choose one of them!:')
+    print("""
+1 = Total Router LA
+2 = Total Router TLK
+3 = Total Router PINS
+4 = Done Upgrade
+""")
+    user_input = int(input('Choose 1 to 4: '))
+    print(user_input)
+    # BCS(df, user_input)
+    if user_input == 1:
+        BCS.getLintasartaRouter(df)
+    elif user_input == 2:
+        BCS.getTelkomRouter(df)
+    elif user_input == 3:
+        BCS.getPINSRouter(df)
+    elif user_input == 4:
+        BCS.getUpgradedRouter(df)
+main()
